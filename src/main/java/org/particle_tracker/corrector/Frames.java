e7fbf95fd4f6b6ee86f8486dc21032ad377fb3a0
MandelWarrior <alabamawarriorxd@gmail.com>
2019-09-09 15:30:12 -0300

    Se removieron import no usados

--- src/main/java/org/particle_tracker/corrector/Frames.java
+++ src/main/java/org/particle_tracker/corrector/Frames.java
@@ -3,7 +3,6 @@ package org.particle_tracker.corrector;
 import java.awt.Point;
 import java.io.File;
 import java.io.FileInputStream;
-import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.InputStreamReader;
