a2498c1efb03b4e90f1b63f6fd8d341ca615b38a
Alan Sartorio <alan42ga@hotmail.com>
2021-04-07 14:53:42 -0300

    Fixed zooming and updated libraries

--- src/main/java/org/particle_tracker/corrector/ZoomController.java
+++ src/main/java/org/particle_tracker/corrector/ZoomController.java
@@ -11,8 +11,8 @@ import java.awt.geom.AffineTransform;
 import java.awt.geom.NoninvertibleTransformException;
 import java.util.ArrayList;
 import java.util.List;
-import java.util.logging.Level;
-import java.util.logging.Logger;
+
+import java.awt.geom.Point2D;
 
 /**
  *
@@ -20,17 +20,22 @@ import java.util.logging.Logger;
  */
 public class ZoomController {
 
-    Point center;
+    Point2D center;
     float scale = 1;
     static float scaleIncrement = 1.2f;
     static float scaleMovement = 0.2f;
     private List<ZoomChangeListener> listeners = new ArrayList<>();
-    public Dimension windowSize;
-    private Point dragStartingPosition;
+    private Dimension windowSize;
+    private Point2D dragStartingPosition;
 
     public ZoomController(Dimension windowSize, Point center) {
         this.windowSize = windowSize;
         this.center = center;
+    }
+
+    void setWindowSize(Dimension windowSize) {
+        this.windowSize = windowSize;
+        callListeners();
     }
     
     void startDrag(Point mousePos) {
@@ -38,9 +43,9 @@ public class ZoomController {
     }
     
     void updateDrag(Point mousePos) {
-        Point imgPoint = screenToImage(mousePos);
+        Point2D imgPoint = screenToImage(mousePos);
         
-        center.translate(dragStartingPosition.x - imgPoint.x, dragStartingPosition.y - imgPoint.y);
+        center.setLocation(center.getX() + dragStartingPosition.getX() - imgPoint.getX(), center.getY() + dragStartingPosition.getY() - imgPoint.getY());
         
         startDrag(mousePos);
         
@@ -56,23 +61,18 @@ public class ZoomController {
     }
 
     void scale(Point mousePos, boolean zoomIn) {
-
         int movement = zoomIn ? 1 : -1;
-        Point imgPoint = screenToImage(mousePos);
+        Point scr = mousePos;
+        float Sb = (float) (scale * Math.pow(scaleIncrement, movement));
 
-        imgPoint.translate(-center.x, -center.y);
-        imgPoint.setLocation(movement * imgPoint.x * scaleMovement, movement * imgPoint.y * scaleMovement);
-        imgPoint.translate(center.x, center.y);
+        Point2D img = screenToImage(scr);
 
-        Point newPoint = imgPoint;
+        double newX = (Sb * img.getX() + windowSize.width / 2 - scr.x) / Sb;
+        double newY = (Sb * img.getY() + windowSize.height / 2 - scr.y) / Sb;
+        
+        center.setLocation(newX, newY);
 
-        center.setLocation(newPoint);
-
-        if (zoomIn) {
-            scale *= scaleIncrement;
-        } else {
-            scale /= scaleIncrement;
-        }
+        scale = Sb;
 
         callListeners();
     }
@@ -90,9 +90,9 @@ public class ZoomController {
         listeners.forEach(listener -> listener.onZoomChange(getTransform()));
     }
     
-    Point screenToImage(Point screenPos) {
+    Point2D screenToImage(Point screenPos) {
         try {
-            Point imgPoint = new Point();
+            Point2D imgPoint = new Point();
             getTransform().inverseTransform(screenPos, imgPoint);
             return imgPoint; 
         } catch (NoninvertibleTransformException ex) {
@@ -101,7 +101,7 @@ public class ZoomController {
         return null;
     }
     
-    Point imageToScreen(Point imagePos) {
+    Point imageToScreen(Point2D imagePos) {
         Point screenPoint = new Point();
         getTransform().transform(imagePos, screenPoint);
         return screenPoint;
@@ -111,9 +111,8 @@ public class ZoomController {
         AffineTransform transform = new AffineTransform();
         
         transform.translate(windowSize.width / 2, windowSize.height / 2);
-        
         transform.scale(scale, scale);
-        transform.translate(-center.x, -center.y);
+        transform.translate(-center.getX(), -center.getY());
 
         return transform;
     }
