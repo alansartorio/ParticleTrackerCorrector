497baa277b4d91f7a6ca172b04bac4484a217e24
Alan Sartorio <alan42ga@hotmail.com>
2023-10-31 10:09:09 -0300

    Add debian package builder

---
+++ debian-package.sh
@@ -0,0 +1,32 @@
+#!/usr/bin/env bash
+
+package=particle-tracker-corrector
+mkdir "$package"
+
+mkdir -p "$package"/usr/{share/{applications,icons/hicolor/scalable,java},lib/jni}
+mkdir "$package"/DEBIAN
+
+cat <<"EOF" > "$package"/DEBIAN/control
+Package: particle-tracker-corrector
+Description: Program for manually tracking particles in videos
+Version: 4.1.0
+Maintainer: Alan Sartorio
+Architecture: all
+Depends: libopencv4.5-java, libcommons-csv-java
+EOF
+
+mvn package
+cp target/particle-tracker-corrector-*.jar "$package"/usr/share/java/particle-tracker-corrector.jar
+
+cat <<"EOF" > "$package"/usr/share/applications/particle-tracker-corrector.desktop
+[Desktop Entry]
+Name=Particle Tracker Corrector
+Exec=java -jar /usr/share/java/particle-tracker-corrector.jar
+Type=Application
+Categories=Utility
+Icon=particle-tracker-corrector
+EOF
+
+cp icon.svg "$package"/usr/share/icons/hicolor/scalable/particle-tracker-corrector.svg
+
+ln -s /usr/lib/jni/libopencv_java454d.so "$package"/usr/lib/jni/libopencv_java454.so
